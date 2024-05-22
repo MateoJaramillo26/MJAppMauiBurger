f19b96577fda7d1adcc518757bd82716ff4e2820
MateoJaramillo26 <105684625+MateoJaramillo26@users.noreply.github.com>
2024-05-22 11:47:04 -0500

    Construido el front

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,14 +1,32 @@
-﻿namespace MJAppMauiBurger
+﻿using MJAppMauiBurger.Models;
+using Newtonsoft.Json;
+namespace MJAppMauiBurger
 {
     public partial class MainPage : ContentPage
     {
+
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-      
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:7271/api/");
+            var response = client.GetAsync("burger").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var burgers = response.Content.ReadAsStringAsync().Result;
+                var burgersList =
+               JsonConvert.DeserializeObject<List<MJBurger>>(burgers);
+                listView.ItemsSource = burgersList;
+            }
+        }
+
     }
+
+
 
 }
